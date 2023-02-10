9873f0b6bd7760f3573358bd36a6e27810690566
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:13:56 +0100

    Add PointyTopHexagonDirection enum

---
+++ Runtime/PointyTopHexagonDirection.cs
@@ -0,0 +1,12 @@
+namespace Osryden.HexagonFramework
+{
+    public enum PointyTopHexagonDirection : uint
+    {
+        Northeast = HexagonDirection.One,
+        East = HexagonDirection.Two,
+        Southeast = HexagonDirection.Three,
+        Southwest = HexagonDirection.Four,
+        West = HexagonDirection.Five,
+        Northwest = HexagonDirection.Six
+    }
+}
