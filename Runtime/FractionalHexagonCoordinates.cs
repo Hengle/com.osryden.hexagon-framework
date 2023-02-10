363f00d1053c007b84240859bd425fd66fa411c6
Oscar Rydén <osryden+github@protonmail.com>
2023-02-11 00:23:32 +0100

    Add FractionalHexagonCoordinates struct

---
+++ Runtime/FractionalHexagonCoordinates.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UnityEngine;
+
+namespace Osryden.HexagonFramework
+{
+    [Serializable]
+    public struct FractionalHexagonCoordinates : IHexagonCoordinates<float>, IEquatable<FractionalHexagonCoordinates>
+    {
+        [SerializeField] private float m_Q;
+        [SerializeField] private float m_R;
+
+        public FractionalHexagonCoordinates(float q, float r)
+        {
+            m_Q = q;
+            m_R = r;
+        }
+
+        public static FractionalHexagonCoordinates Origin { get; } = new FractionalHexagonCoordinates(0, 0);
+
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0: return Q;
+                    case 1: return R;
+                    case 2: return S;
+                    default:
+                        throw new IndexOutOfRangeException($"Invalid index: {index}!");
+                }
+            }
+        }
+
+        public float Q => m_Q;
+        public float R => m_R;
+        public float S => -Q + R;
+        public float Length => (Mathf.Abs(Q) + Mathf.Abs(R) + Mathf.Abs(S)) / 2;
+
+        public static float Distance(FractionalHexagonCoordinates a, FractionalHexagonCoordinates b)
+        {
+            return (a - b).Length;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is not FractionalHexagonCoordinates)
+            {
+                return false;
+            }
+
+            return base.Equals((FractionalHexagonCoordinates)other);
+        }
+
+        public bool Equals(FractionalHexagonCoordinates other)
+        {
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return Q.GetHashCode() ^ (R.GetHashCode() << 2);
+        }
+
+        public override string ToString()
+        {
+            return $"({Q}, {R}, {S})";
+        }
+
+        public static FractionalHexagonCoordinates operator +(FractionalHexagonCoordinates left, FractionalHexagonCoordinates right)
+        {
+            return new FractionalHexagonCoordinates(left.Q + right.Q, left.R + right.R);
+        }
+
+        public static FractionalHexagonCoordinates operator -(FractionalHexagonCoordinates left, FractionalHexagonCoordinates right)
+        {
+            return new FractionalHexagonCoordinates(left.Q - right.Q, left.R - right.R);
+        }
+
+        public static FractionalHexagonCoordinates operator *(float left, FractionalHexagonCoordinates right)
+        {
+            return new FractionalHexagonCoordinates(left * right.Q, left * right.R);
+        }
+
+        public static FractionalHexagonCoordinates operator *(FractionalHexagonCoordinates left, float right)
+        {
+            return new FractionalHexagonCoordinates(left.Q * right, left.R * right);
+        }
+
+        public static bool operator ==(FractionalHexagonCoordinates left, FractionalHexagonCoordinates right)
+        {
+            return (left.Q == right.Q) && (left.R == right.R);
+        }
+
+        public static bool operator !=(FractionalHexagonCoordinates left, FractionalHexagonCoordinates right)
+        {
+            return !(left == right);
+        }
+
+        public static implicit operator FractionalHexagonCoordinates(HexagonCoordinates value)
+        {
+            return new HexagonCoordinates(value.Q, value.R);
+        }
+    }
+}
