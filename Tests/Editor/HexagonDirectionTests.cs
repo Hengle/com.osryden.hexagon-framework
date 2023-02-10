2630cc98097fc35b0908a9c52bf191ac666310b1
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:42:09 +0100

    Add test for number of enum members in HexagonDirection

---
+++ Tests/Editor/HexagonDirectionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Osryden.HexagonFramework.Tests
+{
+    [TestFixture]
+    public class HexagonDirectionTests
+    {
+        [Test]
+        public void TestNumberOfMembers()
+        {
+            int expectedNumberOfMembers = 6;
+            int actualNumberOfMembers = Enum.GetNames(typeof(HexagonDirection)).Length;
+            Assert.AreEqual(expectedNumberOfMembers, actualNumberOfMembers);
+        }
+    }
+}
