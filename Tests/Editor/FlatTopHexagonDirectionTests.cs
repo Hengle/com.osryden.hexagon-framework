5ba624eef847af1e66ff9c39774db96f85592e49
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:32:49 +0100

    Add tests for FlatTopHexagonDirection values

---
+++ Tests/Editor/FlatTopHexagonDirectionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Osryden.HexagonFramework.Tests
+{
+    [TestFixture]
+    public class FlatTopHexagonDirectionTests
+    {
+        [TestCase(FlatTopHexagonDirection.North, HexagonDirection.One)]
+        [TestCase(FlatTopHexagonDirection.Northeast, HexagonDirection.Two)]
+        [TestCase(FlatTopHexagonDirection.Southeast, HexagonDirection.Three)]
+        [TestCase(FlatTopHexagonDirection.South, HexagonDirection.Four)]
+        [TestCase(FlatTopHexagonDirection.Southwest, HexagonDirection.Five)]
+        [TestCase(FlatTopHexagonDirection.Northwest, HexagonDirection.Six)]
+        public void TestFlatTopHexagonDirectionValues(FlatTopHexagonDirection flatTopDirection, HexagonDirection direction)
+        {
+            Assert.AreEqual((int)flatTopDirection, (int)direction);
+        }
+    }
+}
