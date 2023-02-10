d97e17820a06ab9cd9bedac0c139e3cfdc7e7ada
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:12:34 +0100

    Add HexagonDirection enum

---
+++ Runtime/HexagonDirection.cs
@@ -0,0 +1,12 @@
+﻿namespace Osryden.HexagonFramework
+{
+    public enum HexagonDirection : uint
+    {
+        One = 0,
+        Two = 1,
+        Three = 2,
+        Four = 3,
+        Five = 4,
+        Six = 5
+    }
+}
