a1f6e87d5f7d7d22b36f267bdd628a4be970cecd
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:51:07 +0100

    Add tests for HexagonDirection values

--- Tests/Editor/HexagonDirectionTests.cs
+++ Tests/Editor/HexagonDirectionTests.cs
@@ -6,6 +6,17 @@ namespace Osryden.HexagonFramework.Tests
     [TestFixture]
     public class HexagonDirectionTests
     {
+        [TestCase(HexagonDirection.One, 0)]
+        [TestCase(HexagonDirection.Two, 1)]
+        [TestCase(HexagonDirection.Three, 2)]
+        [TestCase(HexagonDirection.Four, 3)]
+        [TestCase(HexagonDirection.Five, 4)]
+        [TestCase(HexagonDirection.Six, 5)]
+        public void TestHexagonDirectionValues(HexagonDirection direction, int value)
+        {
+            Assert.AreEqual((int)direction, value);
+        }
+
         [Test]
         public void TestNumberOfMembers()
         {
