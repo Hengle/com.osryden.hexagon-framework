4dbd456b3be7dd94a02f2fca3ea15f77b9a90896
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 22:43:51 +0100

    Add HexagonCoordinates struct

---
+++ Runtime/HexagonCoordinates.cs
@@ -0,0 +1,100 @@
+using System;
+using UnityEngine;
+
+namespace Osryden.HexagonFramework
+{
+    [Serializable]
+    public struct HexagonCoordinates : IHexagonCoordinates, IEquatable<HexagonCoordinates>
+    {
+        [SerializeField] private int m_Q;
+        [SerializeField] private int m_R;
+
+        public HexagonCoordinates(int q, int r)
+        {
+            m_Q = q;
+            m_R = r;
+        }
+
+        public static HexagonCoordinates Origin { get; } = new HexagonCoordinates(0, 0);
+
+        public int this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0: return Q;
+                    case 1: return R;
+                    case 2: return S;
+                    default:
+                        throw new IndexOutOfRangeException($"Invalid index: {index}!");
+                }
+            }
+        }
+
+        public int Q => m_Q;
+        public int R => m_R;
+        public int S => -Q + R;
+        public int Magnitude => (Mathf.Abs(Q) + Mathf.Abs(R) + Mathf.Abs(S)) / 2;
+
+        public static int Distance(HexagonCoordinates a, HexagonCoordinates b)
+        {
+            return (a - b).Magnitude;
+        }
+
+        public override bool Equals(object other)
+        {
+            if (other is not HexagonCoordinates)
+            {
+                return false;
+            }
+
+            return base.Equals((HexagonCoordinates)other);
+        }
+
+        public bool Equals(HexagonCoordinates other)
+        {
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return Q.GetHashCode() ^ (R.GetHashCode() << 2);
+        }
+
+        public override string ToString()
+        {
+            return $"({Q}, {R}, {S})";
+        }
+
+        public static HexagonCoordinates operator +(HexagonCoordinates left, HexagonCoordinates right)
+        {
+            return new HexagonCoordinates(left.Q + right.Q, left.R + right.R);
+        }
+
+        public static HexagonCoordinates operator -(HexagonCoordinates left, HexagonCoordinates right)
+        {
+            return new HexagonCoordinates(left.Q - right.Q, left.R - right.R);
+        }
+
+        public static HexagonCoordinates operator *(int left, HexagonCoordinates right)
+        {
+            return new HexagonCoordinates(left * right.Q, left * right.R);
+        }
+
+        public static HexagonCoordinates operator *(HexagonCoordinates left, int right)
+        {
+            return new HexagonCoordinates(left.Q * right, left.R * right);
+        }
+
+        public static bool operator ==(HexagonCoordinates left, HexagonCoordinates right)
+        {
+            return (left.Q == right.Q) && (left.R == right.R);
+        }
+
+        public static bool operator !=(HexagonCoordinates left, HexagonCoordinates right)
+        {
+            return !(left == right);
+        }
+    }
+}
