58b6b7977f96cd4f7a1cf81a12cc46e854f5cd34
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 23:13:46 +0100

    Add FlatTopHexagonDirection enum

---
+++ Runtime/FlatTopHexagonDirection.cs
@@ -0,0 +1,12 @@
+﻿namespace Osryden.HexagonFramework
+{
+    public enum FlatTopHexagonDirection : uint
+    {
+        North = HexagonDirection.One,
+        Northeast = HexagonDirection.Two,
+        Southeast = HexagonDirection.Three,
+        South = HexagonDirection.Four,
+        Southwest = HexagonDirection.Five,
+        Northwest = HexagonDirection.Six
+    }
+}
