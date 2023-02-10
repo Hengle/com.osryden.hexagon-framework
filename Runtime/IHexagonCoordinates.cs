851103cc63c21d911ba393ad7245337dac7f6ddd
Oscar Rydén <osryden+github@protonmail.com>
2023-02-10 22:33:46 +0100

    Add IHexagonCoordinates interface

---
+++ Runtime/IHexagonCoordinates.cs
@@ -0,0 +1,10 @@
+namespace Osryden.HexagonFramework
+{
+    public interface IHexagonCoordinates
+    {
+        int Q { get; }
+        int R { get; }
+        int S { get; }
+        int Magnitude { get; }
+    }
+}
