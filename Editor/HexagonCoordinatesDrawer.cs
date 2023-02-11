48bda003235680ca37f72789b9d6b165e7a97322
Oscar Rydén <osryden+github@protonmail.com>
2023-02-12 00:16:04 +0100

    Remove HexagonCoordinatesDrawerBase class

--- Editor/HexagonCoordinatesDrawer.cs
+++ Editor/HexagonCoordinatesDrawer.cs
@@ -1,7 +1,30 @@
 using UnityEditor;
+using UnityEngine;
 
 namespace Osryden.HexagonFramework.Editor
 {
     [CustomPropertyDrawer(typeof(HexagonCoordinates))]
-    public class HexagonCoordinatesDrawer : HexagonCoordinatesDrawerBase<int> { }
+    public class HexagonCoordinatesDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            HexagonCoordinates coordinates = (HexagonCoordinates)fieldInfo.GetValue(property.serializedObject.targetObject);
+
+            SerializedProperty q = property.FindPropertyRelative("m_Q");
+            SerializedProperty r = property.FindPropertyRelative("m_R");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Keyboard), label);
+
+            EditorGUIUtility.labelWidth = 13;
+
+            float width = (position.width - 10) / 3;
+            EditorGUI.PropertyField(new Rect(position.x, position.y, width, position.height), q, new GUIContent(nameof(coordinates.Q)));
+            EditorGUI.PropertyField(new Rect(position.x + width + 5, position.y, width, position.height), r, new GUIContent(nameof(coordinates.R)));
+
+            using (new EditorGUI.DisabledGroupScope(true))
+            {
+                EditorGUI.TextField(new Rect(position.x + (width * 2) + 10, position.y, width, position.height), new GUIContent(nameof(coordinates.S)), coordinates.S.ToString());
+            }
+        }
+    }
 }
