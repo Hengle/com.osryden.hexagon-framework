c27fa4d1f864f4523f91f8ea033053e1bcb2d23a
Oscar Rydén <osryden+github@protonmail.com>
2023-02-12 03:31:00 +0100

    Add documentation for PointyTopHexagonDirection enum

--- Runtime/PointyTopHexagonDirection.cs
+++ Runtime/PointyTopHexagonDirection.cs
@@ -1,12 +1,38 @@
 namespace Osryden.HexagonFramework
 {
+    /// <summary>
+    /// The directions of a pointy-topped hexagon.
+    /// </summary>
     public enum PointyTopHexagonDirection
     {
+        /// <summary>
+        /// Represents the northeast direction of a pointy-topped hexagon.
+        /// </summary>
         Northeast = HexagonDirection.One,
+
+        /// <summary>
+        /// Represents the east direction of a pointy-topped hexagon.
+        /// </summary>
         East = HexagonDirection.Two,
+
+        /// <summary>
+        /// Represents the southeast direction of a pointy-topped hexagon.
+        /// </summary>
         Southeast = HexagonDirection.Three,
+
+        /// <summary>
+        /// Represents the southwest direction of a pointy-topped hexagon.
+        /// </summary>
         Southwest = HexagonDirection.Four,
+
+        /// <summary>
+        /// Represents the west direction of a pointy-topped hexagon.
+        /// </summary>
         West = HexagonDirection.Five,
+
+        /// <summary>
+        /// Represents the northwest direction of a pointy-topped hexagon.
+        /// </summary>
         Northwest = HexagonDirection.Six
     }
 }
