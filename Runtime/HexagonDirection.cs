96c0dc5df8e85a803b513a624cf54252828eb466
Oscar Rydén <osryden+github@protonmail.com>
2023-02-12 03:25:46 +0100

    Add documentation for HexagonDirection enum

--- Runtime/HexagonDirection.cs
+++ Runtime/HexagonDirection.cs
@@ -1,12 +1,38 @@
 ﻿namespace Osryden.HexagonFramework
 {
+    /// <summary>
+    /// The directions of a hexagon.
+    /// </summary>
     public enum HexagonDirection
     {
+        /// <summary>
+        /// Represents the first direction of a hexagon.
+        /// </summary>
         One = 0,
+
+        /// <summary>
+        /// Represents the second direction of a hexagon.
+        /// </summary>
         Two = 1,
+
+        /// <summary>
+        /// Represents the third direction of a hexagon.
+        /// </summary>
         Three = 2,
+
+        /// <summary>
+        /// Represents the fourth direction of a hexagon.
+        /// </summary>
         Four = 3,
+
+        /// <summary>
+        /// Represents the fifth direction of a hexagon.
+        /// </summary>
         Five = 4,
+
+        /// <summary>
+        /// Represents the sixth direction of a hexagon.
+        /// </summary>
         Six = 5
     }
 }
