00e6e7b0ca23e34a7eada68e3f225bfeb5c249d2
Oscar Rydén <osryden+github@protonmail.com>
2023-02-12 03:29:29 +0100

    Add documentation for FlatTopHexagonDirection enum

--- Runtime/FlatTopHexagonDirection.cs
+++ Runtime/FlatTopHexagonDirection.cs
@@ -1,12 +1,38 @@
 ﻿namespace Osryden.HexagonFramework
 {
+    /// <summary>
+    /// The directions of a flat-topped hexagon.
+    /// </summary>
     public enum FlatTopHexagonDirection
     {
+        /// <summary>
+        /// Represents the north direction of a flat-topped hexagon.
+        /// </summary>
         North = HexagonDirection.One,
+
+        /// <summary>
+        /// Represents the northeast direction of a flat-topped hexagon.
+        /// </summary>
         Northeast = HexagonDirection.Two,
+
+        /// <summary>
+        /// Represents the southeast direction of a flat-topped hexagon.
+        /// </summary>
         Southeast = HexagonDirection.Three,
+
+        /// <summary>
+        /// Represents the south direction of a flat-topped hexagon.
+        /// </summary>
         South = HexagonDirection.Four,
+
+        /// <summary>
+        /// Represents the southwest direction of a flat-topped hexagon.
+        /// </summary>
         Southwest = HexagonDirection.Five,
+
+        /// <summary>
+        /// Represents the northwest direction of a flat-topped hexagon.
+        /// </summary>
         Northwest = HexagonDirection.Six
     }
 }
