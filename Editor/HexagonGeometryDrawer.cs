9547ffca9554f4618c20685fd3e8d0547e5c91d9
Oscar Rydén <osryden+github@protonmail.com>
2023-02-12 01:31:11 +0100

    Add custom property drawer for HexagonGeometry

---
+++ Editor/HexagonGeometryDrawer.cs
@@ -0,0 +1,21 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Osryden.HexagonFramework.Editor
+{
+    [CustomPropertyDrawer(typeof(HexagonGeometry))]
+    public class HexagonGeometryDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty orientation = property.FindPropertyRelative("m_Orientation");
+            SerializedProperty size = property.FindPropertyRelative("m_Size");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Keyboard), label);
+
+            EditorGUI.PropertyField(new Rect(position.x, position.y, 84, position.height), orientation, GUIContent.none);
+            EditorGUIUtility.labelWidth = 28;
+            EditorGUI.PropertyField(new Rect(position.x + 84 + 5, position.y, position.width - 84 - 5, position.height), size);
+        }
+    }
+}
