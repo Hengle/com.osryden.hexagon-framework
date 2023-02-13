d987f4dd7841f3287c583704e70d1c0ce4911415
Oscar Rydén <osryden+github@protonmail.com>
2023-02-13 21:50:40 +0100

    Add hexagon diagonal enumerations

--- Runtime/HexagonEnumerations.cs
+++ Runtime/HexagonEnumerations.cs
@@ -42,4 +42,34 @@
         West = HexagonDirection.Five,
         Northwest = HexagonDirection.Six
     }
+
+    public enum HexagonDiagonal
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six
+    }
+
+    public enum FlatTopHexagonDiagonal
+    {
+        Northeast = HexagonDiagonal.One,
+        East = HexagonDiagonal.Two,
+        Southeast = HexagonDiagonal.Three,
+        Southwest = HexagonDiagonal.Four,
+        West = HexagonDiagonal.Five,
+        Northwest = HexagonDiagonal.Six
+    }
+
+    public enum PointyTopHexagonDiagonal
+    {
+        North = HexagonDiagonal.One,
+        Northeast = HexagonDiagonal.Two,
+        Southeast = HexagonDiagonal.Three,
+        South = HexagonDiagonal.Four,
+        Southwest = HexagonDiagonal.Five,
+        Northwest = HexagonDiagonal.Six
+    }
 }
