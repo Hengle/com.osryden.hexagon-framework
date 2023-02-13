5f5cc8167ad13f38ccadb73d098971116d1ac34b
Oscar Rydén <osryden+github@protonmail.com>
2023-02-13 13:59:20 +0100

    Add summaries for most HexagonCoordinates members

--- Runtime/HexagonCoordinates.cs
+++ Runtime/HexagonCoordinates.cs
@@ -13,14 +13,23 @@ namespace Osryden.HexagonFramework
         [SerializeField] private int m_Q;
         [SerializeField] private int m_R;
 
+        /// <summary>
+        /// Creates a <see cref="HexagonCoordinates"/> with the specified <paramref name="q"/> and <paramref name="r"/> coordinates.
+        /// </summary>
         public HexagonCoordinates(int q, int r)
         {
             m_Q = q;
             m_R = r;
         }
 
+        /// <summary>
+        /// Shorthand for <see cref="HexagonCoordinates"/>(0, 0).
+        /// </summary>
         public static HexagonCoordinates Origin { get; } = new HexagonCoordinates(0, 0);
 
+        /// <summary>
+        /// Returns the Q, R, S coordinates using an index of 0, 1, 2 respectively.
+        /// </summary>
         public int this[int index]
         {
             get
@@ -36,16 +45,37 @@ namespace Osryden.HexagonFramework
             }
         }
 
+        /// <summary>
+        /// The Q-axis coordinate.
+        /// </summary>
         public int Q => m_Q;
+
+        /// <summary>
+        /// The R-axis coordinate.
+        /// </summary>
         public int R => m_R;
+
+        /// <summary>
+        /// The S-axis coordinate.
+        /// </summary>
         public int S => -Q + R;
+
+        /// <summary>
+        /// The length of the Q, R, S coordinates.
+        /// </summary>
         public int Length => (Mathf.Abs(Q) + Mathf.Abs(R) + Mathf.Abs(S)) / 2;
 
+        /// <summary>
+        /// Returns the adjacent coordinates in the specified <paramref name="direction"/>.
+        /// </summary>
         public static HexagonCoordinates Adjacent(HexagonDirection direction)
         {
             return Adjacent((FlatTopHexagonDirection)direction);
         }
 
+        /// <summary>
+        /// Returns the adjacent coordinates in the specified <paramref name="direction"/>.
+        /// </summary>
         public static HexagonCoordinates Adjacent(FlatTopHexagonDirection direction)
         {
             switch (direction)
@@ -61,6 +91,9 @@ namespace Osryden.HexagonFramework
             }
         }
 
+        /// <summary>
+        /// Returns the adjacent coordinates in the specified <paramref name="direction"/>.
+        /// </summary>
         public static HexagonCoordinates Adjacent(PointyTopHexagonDirection direction)
         {
             switch (direction)
@@ -76,6 +109,9 @@ namespace Osryden.HexagonFramework
             }
         }
 
+        /// <summary>
+        /// Rounds a fractional coordinates.
+        /// </summary>
         public static HexagonCoordinates Round(FractionalHexagonCoordinates coordinates)
         {
             int q = Mathf.RoundToInt(coordinates.Q);
@@ -98,11 +134,17 @@ namespace Osryden.HexagonFramework
             return new HexagonCoordinates(q, r);
         }
 
+        /// <summary>
+        /// Returns the distance between two coordinates.
+        /// </summary>
         public static int Distance(HexagonCoordinates a, HexagonCoordinates b)
         {
             return (a - b).Length;
         }
 
+        /// <summary>
+        /// Returns the position of the specified <paramref name="coordinates"/> relative to the <paramref name="geometry"/>.
+        /// </summary>
         public static Vector3 Position(HexagonCoordinates coordinates, HexagonGeometry geometry)
         {
             float x = coordinates.Q * geometry.HorizontalDistance;
@@ -111,18 +153,27 @@ namespace Osryden.HexagonFramework
             return HexagonGeometry.RotationAxis(geometry.Orientation, Vector3.up) * new Vector3(x, y, z);
         }
 
+        /// <summary>
+        /// Returns all adjacent coordinates.
+        /// </summary>
         public static IEnumerable<HexagonCoordinates> Adjacents()
         {
             foreach (HexagonDirection direction in (HexagonDirection[])Enum.GetValues(typeof(HexagonDirection)))
                 yield return Adjacent(direction);
         }
 
+        /// <summary>
+        /// Returns all adjacent coordinates of the specified <paramref name="coordinates"/>.
+        /// </summary>
         public static IEnumerable<HexagonCoordinates> Adjacents(HexagonCoordinates coordinates)
         {
             foreach (HexagonCoordinates adjacent in Adjacents())
                 yield return coordinates + adjacent;
         }
 
+        /// <summary>
+        /// Returns all coordinates within the <paramref name="range"/> from the <paramref name="center"/>.
+        /// </summary>
         public static IEnumerable<HexagonCoordinates> Range(HexagonCoordinates center, int range)
         {
             if (range < 1)
@@ -133,6 +184,9 @@ namespace Osryden.HexagonFramework
                     yield return center + new HexagonCoordinates(q, r);
         }
 
+        /// <summary>
+        /// Returns all coordinates on the ring of the specified <paramref name="radius"/> from the <paramref name="center"/>.
+        /// </summary>
         public static IEnumerable<HexagonCoordinates> Ring(HexagonCoordinates center, int radius)
         {
             if (radius < 1)
@@ -145,6 +199,9 @@ namespace Osryden.HexagonFramework
                     yield return coordinates += Adjacent(direction);
         }
 
+        /// <summary>
+        /// Returns all coordinates within the <paramref name="radius"/> starting from the <paramref name="center"/>.
+        /// </summary>
         public static IEnumerable<HexagonCoordinates> Spiral(HexagonCoordinates center, int radius)
         {
             if (radius < 1)
