0453cda055c3edaa8be3bd3ada0d0ac5024b5601
Oscar Rydén <osryden+github@protonmail.com>
2023-02-14 18:17:44 +0100

    Move hexagon direction enums to HexagonDirections.cs file

---
+++ Runtime/Coordinate/HexagonDirections.cs
@@ -0,0 +1,32 @@
+﻿namespace Osryden.HexagonFramework
+{
+    public enum HexagonDirection
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six
+    }
+
+    public enum FlatTopHexagonDirection
+    {
+        North = HexagonDirection.One,
+        Northeast = HexagonDirection.Two,
+        Southeast = HexagonDirection.Three,
+        South = HexagonDirection.Four,
+        Southwest = HexagonDirection.Five,
+        Northwest = HexagonDirection.Six
+    }
+
+    public enum PointyTopHexagonDirection
+    {
+        Northeast = HexagonDirection.One,
+        East = HexagonDirection.Two,
+        Southeast = HexagonDirection.Three,
+        Southwest = HexagonDirection.Four,
+        West = HexagonDirection.Five,
+        Northwest = HexagonDirection.Six
+    }
+}
