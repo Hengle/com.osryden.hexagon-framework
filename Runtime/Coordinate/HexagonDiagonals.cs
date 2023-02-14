ec40290f9be631d417fd0e3824ac2ab1bd6c143d
Oscar Rydén <osryden+github@protonmail.com>
2023-02-14 18:15:39 +0100

    Move hexagon diagonal enums to HexagonDiagonals.cs file

---
+++ Runtime/Coordinate/HexagonDiagonals.cs
@@ -0,0 +1,32 @@
+﻿namespace Osryden.HexagonFramework
+{
+    public enum HexagonDiagonal
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six
+    }
+
+    public enum FlatTopHexagonDiagonal
+    {
+        Northeast = HexagonDiagonal.One,
+        East = HexagonDiagonal.Two,
+        Southeast = HexagonDiagonal.Three,
+        Southwest = HexagonDiagonal.Four,
+        West = HexagonDiagonal.Five,
+        Northwest = HexagonDiagonal.Six
+    }
+
+    public enum PointyTopHexagonDiagonal
+    {
+        North = HexagonDiagonal.One,
+        Northeast = HexagonDiagonal.Two,
+        Southeast = HexagonDiagonal.Three,
+        South = HexagonDiagonal.Four,
+        Southwest = HexagonDiagonal.Five,
+        Northwest = HexagonDiagonal.Six
+    }
+}
