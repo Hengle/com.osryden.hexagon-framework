7040b4888c2da5fa96b2dbfb2560f666de6fd27f
Oscar Rydén <osryden+github@protonmail.com>
2023-02-15 15:46:03 +0100

    Add members to IHexagonMap

--- Runtime/Map/HexagonMap.cs
+++ Runtime/Map/HexagonMap.cs
@@ -1,8 +1,24 @@
-﻿namespace Osryden.HexagonFramework
+﻿using System.Collections.Generic;
+
+namespace Osryden.HexagonFramework
 {
     public class HexagonMap<THexagonTile> : IHexagonMap<THexagonTile>
         where THexagonTile : IHexagonTile
     {
+        public THexagonTile this[HexagonCoordinates coordinates]
+        {
+            get => throw new System.NotImplementedException();
+            set => throw new System.NotImplementedException();
+        }
 
+        public int Count => throw new System.NotImplementedException();
+        public ICollection<HexagonCoordinates> Coordinates => throw new System.NotImplementedException();
+        public ICollection<THexagonTile> Tiles => throw new System.NotImplementedException();
+
+        public void Clear() => throw new System.NotImplementedException();
+
+        public bool ContainsCoordinates(HexagonCoordinates coordinates) => throw new System.NotImplementedException();
+
+        public bool TryGetTile(HexagonCoordinates coordinates, out THexagonTile tile) => throw new System.NotImplementedException();
     }
 }
--- Runtime/Map/IHexagonMap.cs
+++ Runtime/Map/IHexagonMap.cs
@@ -1,8 +1,18 @@
-﻿namespace Osryden.HexagonFramework
+﻿using System.Collections.Generic;
+
+namespace Osryden.HexagonFramework
 {
     public interface IHexagonMap<THexagonTile>
         where THexagonTile : IHexagonTile
     {
+        THexagonTile this[HexagonCoordinates coordinates] { get; set; }
 
+        int Count { get; }
+        ICollection<HexagonCoordinates> Coordinates { get; }
+        ICollection<THexagonTile> Tiles { get; }
+
+        void Clear();
+        bool ContainsCoordinates(HexagonCoordinates coordinates);
+        bool TryGetTile(HexagonCoordinates coordinates, out THexagonTile tile);
     }
 }
