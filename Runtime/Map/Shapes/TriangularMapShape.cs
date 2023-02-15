58f6cffded36ede9e53ccb776311198cff0b6a6b
Oscar Rydén <osryden+github@protonmail.com>
2023-02-15 16:56:27 +0100

    Add TriangularMapShape class

---
+++ Runtime/Map/Shapes/TriangularMapShape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Osryden.HexagonFramework
+{
+    [Serializable]
+    public class TriangularMapShape : HexagonMapShape
+    {
+        [SerializeField] private int m_Size;
+        [SerializeField] private HexagonCoordinateAxis m_Axis;
+
+        public TriangularMapShape(HexagonCoordinates origin, int size, HexagonCoordinateAxis axis) : base(origin)
+        {
+            m_Size = size;
+            m_Axis = axis;
+        }
+
+        public int Size => m_Size;
+        public HexagonCoordinateAxis Axis => m_Axis;
+
+        protected override HashSet<HexagonCoordinates> Shape()
+        {
+            var shape = new HashSet<HexagonCoordinates>();
+
+            for (int q = 0; q <= Size; q++)
+                for (int r = 0; r <= Size - q; r++)
+                    shape.Add(HexagonCoordinates.Reflect(new HexagonCoordinates(q, -r), Axis));
+
+            return shape;
+        }
+    }
+}
