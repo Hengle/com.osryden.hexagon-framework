4669f75884b19ab52d8e10159d8a775e814ba5d1
Oscar Rydén <osryden+github@protonmail.com>
2023-02-15 16:32:13 +0100

    Add HexagonalMapShape class

---
+++ Runtime/Map/Shapes/HexagonalMapShape.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Osryden.HexagonFramework
+{
+    [Serializable]
+    public class HexagonalMapShape : HexagonMapShape
+    {
+        [SerializeField] private int m_Size;
+
+        public HexagonalMapShape(HexagonCoordinates origin, int size) : base(origin)
+        {
+            m_Size = size;
+        }
+
+        public int Size => m_Size;
+
+        protected override HashSet<HexagonCoordinates> Shape()
+        {
+            return HexagonCoordinates.Range(HexagonCoordinates.Origin, Size).ToHashSet();
+        }
+    }
+}
