e30da669cfa2b10edb5f3c2ef2bac6fad4024cec
Oscar Rydén <osryden+github@protonmail.com>
2023-02-16 03:16:03 +0100

    Add documentation for HexagonOrientation

--- Runtime/Geometry/HexagonOrientation.cs
+++ Runtime/Geometry/HexagonOrientation.cs
@@ -1,8 +1,18 @@
 ﻿namespace Osryden.HexagonFramework
 {
+    /// <summary>
+    /// Represents the orientations of a hexagon.
+    /// </summary>
     public enum HexagonOrientation
     {
+        /// <summary>
+        /// Represents the flat-top orientation of a hexagon.
+        /// </summary>
         FlatTop,
+
+        /// <summary>
+        /// Represents the pointy-top orientation of a hexagon.
+        /// </summary>
         PointyTop
     }
 }
