86d75a296ce2965acfb331cb4ff3c0e2ba3c3815
Oscar Rydén <osryden+github@protonmail.com>
2023-02-16 03:19:38 +0100

    Add documentation for HexagonCoordinateAxis

--- Runtime/Coordinates/HexagonCoordinateAxis.cs
+++ Runtime/Coordinates/HexagonCoordinateAxis.cs
@@ -1,9 +1,23 @@
 ﻿namespace Osryden.HexagonFramework
 {
+    /// <summary>
+    /// Represents the axes of the hexagon coordinates.
+    /// </summary>
     public enum HexagonCoordinateAxis
     {
+        /// <summary>
+        /// Represents the Q-coordinate axis.
+        /// </summary>
         Q,
+
+        /// <summary>
+        /// Represents the R-coordinate axis.
+        /// </summary>
         R,
+
+        /// <summary>
+        /// Represents the S-coordinate axis.
+        /// </summary>
         S
     }
 }
